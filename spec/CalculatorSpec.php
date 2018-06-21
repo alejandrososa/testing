87b966bd38d4287d2a70cde86d4427f78a8cd649
alejandrososa <elpastorsalomon@gmail.com>
2018-06-21 18:43:13 +0200

    Add first test

--- spec/CalculatorSpec.php
+++ spec/CalculatorSpec.php
@@ -1,15 +1,20 @@
 <?php
 
-namespace spec\Acme;
+namespace spec\App;
 
-use Acme\Calculator;
+use App\Calculator;
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 
 class CalculatorSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
         $this->shouldHaveType(Calculator::class);
+    }
+
+    function it_can_sum()
+    {
+        $this->sum(1, 3)
+            ->shouldBeEqualTo(4);
     }
 }
---
+++ src/Calculator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class Calculator
+{
+    public function sum($argument1, $argument2)
+    {
+        return $argument1 + $argument2;
+    }
+}
